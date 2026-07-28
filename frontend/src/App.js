ea22120d2a2ff6b20510f9b5bf369fda208a69f4
Aiswariya S <aiswariyasuresh1@gmail.com>
2026-07-28 11:39:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Portfolio from "@/pages/Portfolio";
+import Popup from "./components/popup";
 
 function App() {
   return (
